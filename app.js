bfeda3c255b3e512ec503391297d1e97900d1e66
Your Name <feixiang2008126@126.com>
2017-01-09 18:33:21 +0800

    修复首页不能展示的bug

--- app.js
+++ app.js
@@ -30,38 +30,37 @@ app.get('/', function(req, res) {
 
     res.render('index', {
       title: 'node-mongodb 首页',
-      movies: []
+      movies: movies
     })
 
   })
   // res.render('index', {
   //   title: 'node-mongodb 首页',
-  //   movies: []
-    // movies: [{
-    //   title: '机械战警',
-    //   _id: 1,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // },{
-    //   title: '机械战警',
-    //   _id: 2,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // },{
-    //   title: '机械战警',
-    //   _id: 3,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // },{
-    //   title: '机械战警',
-    //   _id: 4,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // },{
-    //   title: '机械战警',
-    //   _id: 5,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // },{
-    //   title: '机械战警',
-    //   _id: 6,
-    //   poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
-    // }]
+  //   movies: [{
+  //     title: '机械战警',
+  //     _id: 1,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   },{
+  //     title: '机械战警',
+  //     _id: 2,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   },{
+  //     title: '机械战警',
+  //     _id: 3,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   },{
+  //     title: '机械战警',
+  //     _id: 4,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   },{
+  //     title: '机械战警',
+  //     _id: 5,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   },{
+  //     title: '机械战警',
+  //     _id: 6,
+  //     poster: 'http://b.hiphotos.baidu.com/exp/w=500/sign=af828be6aaaf2eddd4f149e9bd110102/35a85edf8db1cb1390dfc9f3db54564e93584b98.jpg'
+  //   }]
   // })
 })
 
